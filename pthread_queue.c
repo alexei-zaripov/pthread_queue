8c76803bae5a54141c52e6a9fb8099c4b10aaeb1
Alexei Zaripov <alexei.zaripov@yandex.ru>
2023-04-16 21:55:10 +0300

    First commit. Working version

---
+++ pthread_queue.c
@@ -0,0 +1,136 @@
+// SPDX-License-Identifier: MIT
+
+#include <pthread.h>
+#include <stdlib.h>
+
+// bounded synchronous multi-producer multi-consumer queue on pthreads
+// dependencies: pthread.h, C99 or later.
+// somewhat similar to the bounded channels in go.
+// queues are created with fixed non-zero capacity
+// queues can be marked as "closed" to inform consumers that no more elements will be added to it
+// push will block if queue is full, waiting for an item to be poped
+// pop retrives the oldest element if there is one, otherwise if queue is closed returns 0,
+//     otherwise if queue is empty waits untill an element is inserted or the queue is closed
+// close marks queue as closed, no elements can be added into closed queue 
+
+#ifndef QUEUE
+# error "this file needs QUEUE to be defined"
+#endif
+#define QU1(a, b, c) a
+#define QU2(a, b, c) b
+#define QU3(a, b, c) c
+#define QCAT1(a, b) a ## b
+#define QCAT2(a, b) QCAT1(a, b)
+#define QCAT3(a, b) QCAT2(a, b)
+#define QUEUENAME(a) QU1 a
+#define QUEUEITEM(a) QU2 a
+#define QUEUEPREF_(a) QU3 a
+#define QUEUEPREF QUEUEPREF_(QUEUE)
+#define queue_t QUEUENAME(QUEUE)
+#define qitem_t QUEUEITEM(QUEUE)
+#define queue_init QCAT3(QUEUEPREF, init)
+#define queue_push QCAT3(QUEUEPREF, push)
+#define queue_pop QCAT3(QUEUEPREF, pop)
+#define queue_close QCAT3(QUEUEPREF, close)
+#define queue_drop QCAT3(QUEUEPREF, destroy)
+
+typedef struct {
+	qitem_t *buf;
+	size_t cap;
+	size_t len;
+	size_t begin;
+	int isclosed;
+	pthread_mutex_t mtx;
+	pthread_cond_t cond;
+} queue_t;
+
+int queue_init(queue_t *q, size_t cap) {
+	if (cap == 0) {
+		return 0;
+	}
+	q->buf = malloc(sizeof(qitem_t) * cap);
+	if (q->buf == NULL) {
+		free(q);
+		return 0;
+	}
+	q->cap = cap;
+	q->isclosed = q->len = q->begin = 0;
+	pthread_cond_init(&q->cond, NULL);
+	pthread_mutex_init(&q->mtx, NULL);
+	return 1;
+}
+
+int queue_push(queue_t *q, qitem_t *i) {
+	pthread_mutex_lock(&q->mtx);
+	while (q->len == q->cap && q->isclosed == 0) {
+		pthread_cond_wait(&q->cond, &q->mtx);
+	}
+	if (q->isclosed) {
+		pthread_mutex_unlock(&q->mtx);
+		pthread_cond_broadcast(&q->cond);
+		return 0;
+	}
+	q->buf[(q->begin+q->len) % q->cap] = *i;
+	q->len++;
+	pthread_mutex_unlock(&q->mtx);
+	pthread_cond_broadcast(&q->cond);
+	return 1;
+}
+
+int queue_pop(queue_t *q, qitem_t *i) {
+	pthread_mutex_lock(&q->mtx);
+	// at this point queue can be:
+	//   non-empty and open
+	//   non-empty and closed
+	//   empty and open
+	//   empty and closed
+	// the only state in which it makes sense to wait is "empty and open"
+	while (q->len == 0 && q->isclosed == 0) {
+		pthread_cond_wait(&q->cond, &q->mtx);
+	}
+	if (q->len == 0) {
+		pthread_mutex_unlock(&q->mtx);
+		pthread_cond_broadcast(&q->cond);
+		return 0;
+	}
+	*i = q->buf[q->begin];
+	q->len--;
+	q->begin = (q->begin+1) % q->cap;
+	pthread_mutex_unlock(&q->mtx);
+	pthread_cond_broadcast(&q->cond);
+	return 1;
+}
+
+void queue_close(queue_t *q) {
+	pthread_mutex_lock(&q->mtx);
+	q->isclosed = 1;
+	pthread_mutex_unlock(&q->mtx);
+	pthread_cond_broadcast(&q->cond);
+}
+
+void queue_drop(queue_t *q) {
+	free(q->buf);
+	q->buf = NULL;
+	pthread_mutex_destroy(&q->mtx);
+	pthread_cond_destroy(&q->cond);
+}
+
+#undef QUEUE
+#undef QU1
+#undef QU2
+#undef QU3
+#undef QCAT1
+#undef QCAT2
+#undef QCAT3
+#undef QUEUENAME
+#undef QUEUEITEM
+#undef QUEUEPREF_
+#undef QUEUEPREF
+#undef queue_t
+#undef qitem_t
+#undef queue_init
+#undef queue_push
+#undef queue_pop
+#undef queue_close
+#undef queue_drop
+
---
+++ readme.md
@@ -0,0 +1,85 @@
+synchronous multi-producer multi-consumer bounded queue on pthreads
+
+## About
+This is a generic C implementation of a synchronous bounded queue, similar in semantics to Go's bounded channels.
+Dependencies: C99, <pthread.h>
+
+## Usage
+### Setting up
+Just drop pthread_queue.c and pthread_queue.h if needed in your include path.
+These files contain generic or template code to instantiate which you will need to define QUEUE
+macro, which will contain parameters to the template and include these files. Like this:
+
+```c
+#define QUEUE (my_queue_t, data_t, queue_)
+#include <pthread_queue.c>
+```
+where `(my_queue_t, data_t, queue_)` act as template parameters: first defines the name that queue type will use
+, second defines the name of a type you want to pass through the queue, third defines the prefixes for the
+function names that will be defined/decalred by pthread_queue.c/h . So you can instantiate multiple queues
+as long as they have distinct first and third template parameters.
+
+### API
+struct and function definitions are given here as if `#define QUEUE (queue_t, item_t, queue_)` was used
+
+```c
+typedef struct {
+        // all fields are private
+} queue_t;
+```
+Moving `queue_t` after it has been initialized will result in "undefined behavior" because it contains mutex which are illegal to move as per POSIX
+
+- `int queue_init(queue_t *q, size_t cap)`
+  * initializes queue pointed to by `q` with capacity to hold `cap` elements.
+    note that `queue_t` should not be moved in memory after initialization, as it is UB to move POSIX mutex that `queue_t` contains.
+    returns 1 if initialization was successful, 0 otherwise.
+- `int queue_push(queue_t *q, item_t *i)`
+  * pushes a copy of `*i` into the queue's buffer. if queue is full, blocks until some other thread pops an item or closes the queue.
+    if queue is closed does not push an item and returns 0, otherwise 1.
+- `int queue_pop(queue_t *q, item_t *i)`
+  * pops an oldest item from the queue into `*i` if there is one in queue. blocks until an element is pushed into this queue or the queue is closed.
+    if queue is empty and closed returns 0, otherwise 1.
+- `void queue_close(queue_t *q)`
+  * closes the queue.
+- `void queue_drop(queue_t *q)`
+  * frees resources allocated to the queue.
+
+### Example
+```c
+#include <stdio.h>
+#include <pthread.h>
+#define QUEUE (ichan, int, ichan_)
+#include "pthread_queue.c"
+
+void reader(ichan *c) {
+	int s = 0;
+	int x;
+	while (ichan_pop(c, &x)) {
+		s += x;
+	}
+	printf("sum: %d\n", s);
+}
+
+void writer(ichan *c) {
+	for (int n = 10000; n; n--) {
+		ichan_push(c, &n);
+	}
+	ichan_close(c);
+}
+
+int main(void) {
+	pthread_t rd, wr;
+	ichan c;
+	ichan_init(&c, 10);
+	
+	pthread_create(&rd, NULL, (void*(*)(void*))reader, (void*)&c);
+	pthread_create(&wr, NULL, (void*(*)(void*))writer, (void*)&c);
+	
+	pthread_join(wr, NULL);
+	pthread_join(rd, NULL);
+	
+	ichan_drop(&c);
+	return 0;	
+}
+```
+
